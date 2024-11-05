be7383ea6572bd3116ce5c6bf8530595dd289c2b
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2024-11-05 08:49:50 +0000

    GetAirlineByID

--- routes/airlines.mjs
+++ routes/airlines.mjs
@@ -13,4 +13,38 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Get airline by id
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const [airline] = await pool.query(`SELECT * from Airlines WHERE id = ?`, [
+      id,
+    ]);
+    res.json(airline);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Update airline by id
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, country, email, rental_role } = req.body;
+
+  try {
+    const [result] = await pool.query(
+      `UPDATE Airlines SET name = ?, country = ?, email = ?, rental_role = ? WHERE id = ?`,
+      [name, country, email, rental_role, id]
+    );
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: "Airline not found" });
+    }
+
+    res.json({ message: "Airline updated successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 export default router;
